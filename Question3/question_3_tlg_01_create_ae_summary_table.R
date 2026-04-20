7772a206ae7105eba5310a524bf7df0f85d011a4
Betty Chen <betty.chen@sickkids.ca>
2026-04-19 21:17:01 -0400

    Update Question 3 AE summary table script

--- Question3/question_3_tlg_01_create_ae_summary_table.R
+++ Question3/question_3_tlg_01_create_ae_summary_table.R
@@ -1,6 +1,6 @@
 #-------------------
 #Question_3
-#Last Updated on 2026-04-18
+#Last Updated on 2026-04-19
 #Updated by Betty Chen
 #-------------------
 
@@ -11,8 +11,26 @@
 # Task: 
 # Create a summary table of treatment-emergent adverse events (TEAEs).
 
+setwd("C:/Users/betty chen/Documents/ADS_Programmer_Early_Development_coding_assessment/Question3")
+#------------------
+# Prepare to save log later
+#------------------
+# create output folder if it does not exist
+if (!dir.exists("output")) {
+  dir.create("output")
+}
+
+# close any existing sinks
+while (sink.number() > 0) sink()
+
+# start log
+sink("output/Question3_aesummary_run_log.txt", split = TRUE)
+
+cat("Run started:", as.character(Sys.time()), "\n")
+
+
 #--------------------------------------------------
-# 1.load library and input data (1.1-1.2)
+# 1.load library and input data (2 files)
 #--------------------------------------------------
 library(dplyr)
 library(tidyr)
@@ -26,7 +44,7 @@ adsl <- pharmaverseadam::adsl
 adae <- pharmaverseadam::adae
 
 #--------------------------------------------------
-# 2.Prepare data (2.1)
+# 2.Prepare data (2.1-2.2)
 #-------------------------------------------------
 #2.1 create denominator
 adsl_pop <- adsl%>%
@@ -66,17 +84,10 @@ tbl <- teae %>%
 
 tbl
 
-# -----------------------------------------------------
-# 4. Save outputs
-# -----------------------------------------------------
-gt_tbl <- as_gt(tbl)
-gtsave(gt_tbl, "Treatment_Emergent_AEs_table.html")
-
-
 #--------------------------------------------------
-# 5.QC TEST for Build Table using tbl_hierarchical()
+# 4.QC TEST 
 #--------------------------------------------------
-
+#alternative way 
 tbl2 <- teae %>%
   tbl_hierarchical(
     variables = c(AESOC, AEDECOD),
@@ -95,58 +106,28 @@ tbl2 <- teae %>%
   sort_hierarchical(sort = everything() ~ "descending")
 
 tbl2
+#SAME--> YEAH!
+
+# -----------------------------------------------------
+# 5. Save outputs
+# -----------------------------------------------------
+gt_tbl <- as_gt(tbl)
+gtsave(gt_tbl, "output/Treatment_Emergent_AEs_table.html")
 
 
-# tbl2 <- teae %>%
-#   tbl_hierarchical(
-#     variables = c(AESOC, AEDECOD),
-#     by = ACTARM,
-#     id = USUBJID,
-#     denominator = adsl_pop,
-#     statistic = everything() ~ "{n} ({p}%)",
-#     overall_row = TRUE,
-#     label = list(
-#       "..ard_hierarchical_overall.." ~ "Treatment Emergent AEs"
-#     )
-#   ) %>%
-#   sort_hierarchical(sort = everything() ~ "descending") %>%
-#   modify_header(
-#     label ~ "**Primary System Organ Class**<br>**Reported Term for the Adverse Event**"
-#   )
-# 
-# tbl2
+cat("Output dataset saved to: output/Treatment_Emergent_AEs_table.html\n")
 
-tbl2
+cat("Preview of AEs summary table body:\n")
+print(head(tbl$table_body, 20))
 
-tbl2 <- adae |>
-  tbl_hierarchical(
-    variables = c(AESOC, AEDECOD),
-    by = TRT01A,
-    id = USUBJID,
-    denominator = adsl,
-    overall_row = TRUE,
-    label = "..ard_hierarchical_overall.." ~ "Treatment Emergent AEs",
-    #statistic = everything() ~ "{n} ({p}%)",
-    #overall_row = TRUE,
-  )#%>%
-  # add_overall(last = TRUE) %>%
-  # sort_hierarchical(sort = everything() ~ "descending")
+cat("\nClass of table object:\n")
+print(class(tbl))
 
+cat("Table saved successfully\n")
+cat("Run ended:", as.character(Sys.time()), "\n")
 
-tbl2
+# close log
+sink()
 
-
-#correct ewith picture 1 but not right with picure 2 
-tbl5 <- adae |>
-  tbl_hierarchical(
-    variables = c(AESOC, AEDECOD),
-    by = TRT01A,
-    id = USUBJID,
-    denominator = adsl,
-    overall_row = TRUE,
-    label = "..ard_hierarchical_overall.." ~ "Any SAE"
-  )
-
-tbl5
 
 
